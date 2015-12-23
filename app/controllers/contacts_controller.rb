9af35af33200154196355082c8528487075ef858
Tommy Meyer <tlmeyer942@gmail.com>
2015-12-23 03:04:05 -0700

    Added validations and bootstrap to form. Adjusted CSS

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,12 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 
 		if @contact.save
-			redirect_to new_contact_path, notice: "Message sent."
+			flash[:success] = "Message sent."
+			redirect_to new_contact_path
 		else
-			redirect_to new_contact_path, notice: "Error occurred."
+			#Danger is Bootstrap for Error.
+			flash[:danger] = "Error occurred, message has not been sent."
+			redirect_to new_contact_path
 		end
 	end
 
