edcbdcae03e4619092669bdbb4bdff92a0f1c84c
Tommy Meyer <tlmeyer942@gmail.com>
2016-01-10 04:59:06 -0700

    Removed plans from database, models, and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 20151227204904) do
     t.datetime "updated_at"
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string  "name"
-    t.decimal "price"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -39,7 +34,6 @@ ActiveRecord::Schema.define(version: 20151227204904) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
